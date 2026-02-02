5a6cc9bc7dd82a048c6fb455b2da0b36cea9f4a1
Philipp Koller <philipp.kooller@googlemail.com>
2026-02-02 11:17:08 +0100

    Update Loader

--- components/loading/Loader.tsx
+++ components/loading/Loader.tsx
@@ -4,6 +4,7 @@ import { useEffect, useState } from 'react'
 import { preloadAssets } from '@/util/preloadAssets'
 // import { assets } from '@/data/fileData'
 import assets from '@/public/assets.json'
+import styles from './Loader.module.css'
 
 
 
@@ -18,49 +19,49 @@ export default function Preloader({ onDone }: { onDone: () => void }) {
   }, [onDone])
 
   return (
-    <div style={styles.overlay}>
-      <div style={styles.bar}>
+    <div className={styles.overlay}>
+      <div className={styles.bar}>
         <div
-          style={{
-            ...styles.fill,
-            transform: `scaleX(${progress})`
-          }}
+        className={styles.fill}
+        style={{
+          transform: `scaleX(${progress})`
+        }}
         />
       </div>
-      <div style={styles.label}>
+      <div className={styles.label}>
         {Math.round(progress * 100)}%
       </div>
     </div>
   )
 }
 
-const styles = {
-  overlay: {
-    position: 'fixed',
-    inset: 0,
-    background: '#000',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-    flexDirection: 'column',
-    zIndex: 9999,
-  },
-  bar: {
-    width: '50vw',
-    height: 4,
-    background: '#222',
-    overflow: 'hidden',
-  },
-  fill: {
-    height: '100%',
-    background: '#fff',
-    transformOrigin: '0 0',
-    transition: 'transform 0.2s ease',
-  },
-  label: {
-    marginTop: 12,
-    color: '#888',
-    fontSize: 12,
-    fontFamily: 'monospace',
-  },
-} as const
+// const styles = {
+//   overlay: {
+//     position: 'fixed',
+//     inset: 0,
+//     background: '#000',
+//     display: 'flex',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//     flexDirection: 'column',
+//     zIndex: 9999,
+//   },
+//   bar: {
+//     width: '50vw',
+//     height: 4,
+//     background: '#222',
+//     overflow: 'hidden',
+//   },
+//   fill: {
+//     height: '100%',
+//     background: '#fff',
+//     transformOrigin: '0 0',
+//     transition: 'transform 0.2s ease',
+//   },
+//   label: {
+//     marginTop: 12,
+//     color: '#888',
+//     fontSize: 12,
+//     fontFamily: 'monospace',
+//   },
+// } as const
